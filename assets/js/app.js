6c31c71255548c6c8cfaaeaba3ff8c4e2a9c19a8
Geraldine Chauca <gchaucae@gmail.com>
2017-04-04 12:10:36 -0500

    Correcciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,22 +5,20 @@ var retratos = document.getElementsByClassName('img');
 var clase1 = document.getElementById("Chile");
 var clase2 = document.getElementById("Lima1");
 var clase3 = document.getElementById("Lima2");
-var select = document.getElementById("promomciones");
+var select = document.getElementById("promociones");
 
-
+console.log(select);
 select.onchange = function (){
   if(select.value == "Chile"){
     clase1.style.display = "block";
     clase2.style.display = "none";
     clase3.style.display = "none";
-    div.style.display = "none";
   }
 
   else if(select.value == "Lima"){
     clase1.style.display = "none";
     clase2.style.display = "block";
     clase3.style.display = "none";
-    div.style.display = "none";
 
   }
 
@@ -28,7 +26,6 @@ select.onchange = function (){
     clase1.style.display = "none";
     clase2.style.display = "none";
     clase3.style.display = "block";
-    div.style.display = "none";
 
   }
 
@@ -36,7 +33,6 @@ select.onchange = function (){
     clase1.style.display = "none";
     clase2.style.display = "none";
     clase3.style.display = "none";
-    div.style.display = "block";
   }
 }
 });
